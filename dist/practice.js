0dca1bd8b9fb6a484e06e05b7e054d7f973c4b7d
jake <guswns1853@gmail.com>
2020-04-02 22:40:19 +0900

    interface practice

--- dist/practice.js
+++ dist/practice.js
@@ -1,3 +1,39 @@
 "use strict";
-var message = 'hello world';
-console.log(message);
+// function sum(x: number, y: number): number {
+//   return 1 + 2;
+// }
+//
+// sum(1, 2);
+//
+// function sumArray(numbers: number[]): number {
+//   return numbers.reduce((acc, current) => acc + current, 0);
+// }
+//
+// const total = sumArray([1, 2, 3, 4, 5]);
+//
+// function returnNothing(): void {
+//  console.log('I am just saying hello world');
+// }
+var Circle = /** @class */ (function () {
+    function Circle(radius) {
+        this.radius = radius;
+    }
+    Circle.prototype.getArea = function () {
+        return this.radius * this.radius * Math.PI;
+    };
+    return Circle;
+}());
+var Rectangle = /** @class */ (function () {
+    function Rectangle(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    Rectangle.prototype.getArea = function () {
+        return this.width * this.height;
+    };
+    return Rectangle;
+}());
+var shapes = [new Circle(5), new Rectangle(10, 5)];
+shapes.forEach(function (shape) {
+    console.log(shape.getArea());
+});
--- src/practice.ts
+++ src/practice.ts
@@ -1,2 +1,50 @@
-const message: string = 'hello world';
-console.log(message);
+// function sum(x: number, y: number): number {
+//   return 1 + 2;
+// }
+//
+// sum(1, 2);
+//
+// function sumArray(numbers: number[]): number {
+//   return numbers.reduce((acc, current) => acc + current, 0);
+// }
+//
+// const total = sumArray([1, 2, 3, 4, 5]);
+//
+// function returnNothing(): void {
+//  console.log('I am just saying hello world');
+// }
+
+// interface
+interface Shape {
+  getArea(): number;
+}
+
+class Circle implements Shape {
+  radius: number;
+
+  constructor(radius: number) {
+    this.radius = radius;
+  }
+
+  getArea(): number {
+    return this.radius * this.radius * Math.PI;
+  }
+}
+
+class Rectangle implements Shape {
+  width: number;
+  height: number;
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
+
+shapes.forEach(shape => {
+  console.log(shape.getArea());
+});
